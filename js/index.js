b88461e98c91bac0292a3a88c44b352a04474c64
Zani231089 <231089@virtualwindow.co.za>
2023-05-28 20:48:36 +0200

    Javascript finial

--- js/index.js
+++ js/index.js
@@ -3,18 +3,18 @@ let allStudents = []
 registerAccount = () => {
     let first = document.getElementById("first").value
     let email = document.getElementById("email").value
-    let number = document.getElementById("number").value
+    let number = document.getElementById("contact").value
     let subject = document.getElementById("subject").value
 
-    if(defaultCheck1 = document.getElementById(defaultCheck1)){
+    if(defaultCheck = document.getElementById(defaultCheck)){
         allStudents.push({
             studentFirst: first,
             studentEmail: email,
             studentSubject: subject,
-            studentNumber: number
+            studentNumber: contact
         })
 
-        alert("Welcome " + first + " !" + "Thank you for your intrest in" + subject + "!" + "We will contact you via the email provided:" + email + "," + "or alternatively on the phone number" + number)
+        alert("Welcome " + first + " !" + "Thank you for your interest in" + subject + "!" + "We will contact you via the email provided:" + email + "," + "or alternatively on the phone number" + contact)
     } else{
         alert("Well, well...Why don't you sign up for the newsletter..you don't seem to interested" + first + "If you want to sign up , you better check that newsletter box!")
     }
